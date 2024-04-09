21e72a5650c007a0e8a986bb6f25f649f7a2b259
xixihaga <czm9790_cn@hotmail.com>
2024-04-09 21:59:38 +0800

    chapter11 search already

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function handleSearch(term: string) {
     
-      console.log('Searching... ${term}')
+      console.log(`Searching... ${term}`)
       const params = new URLSearchParams(searchParams);
       if (term) {
         params.set('quary', term);
