c89f625a3524412b48da65ce5e0260eac60853d2
xixihaga <czm9790_cn@hotmail.com>
2024-04-09 22:01:07 +0800

    chapter11 search debounce test

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,17 +9,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    
-      console.log(`Searching... ${term}`)
-      const params = new URLSearchParams(searchParams);
-      if (term) {
-        params.set('quary', term);
-      } else {
-        params.delete('query')
-      }
-      replace(`${pathname}?${params.toString()}`);
-  }
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
