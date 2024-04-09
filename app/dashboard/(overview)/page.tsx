6ac6ab456ddbaaf5dba32e0b6ef3d976f87a8c4a
xixihaga <czm9790_cn@hotmail.com>
2024-04-09 16:13:33 +0800

    Chapter 11

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,7 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons
 
 export default async function Page() {
     // const latestInvoices = await fetchLatestInvoices();
-    const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await (await fetchCardData());
+    const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await (await fetchCardData());
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -20,10 +20,10 @@ export default async function Page() {
                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
                 <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
                 <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+                    title="Total Customers"
+                    value={numberOfCustomers}
+                    type="customers"
+                />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
