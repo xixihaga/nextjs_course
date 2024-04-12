9f734f782f4b4615e61d1ae8764e38f6a6df3789
xixihaga <czm9790_cn@hotmail.com>
2024-04-12 16:34:05 +0800

    Chapter 12 +++

--- app/lib/actons.ts
+++ app/lib/actons.ts
@@ -1,9 +1,9 @@
 'use server'
 
 import { z } from 'zod';
-import {sql} from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import{ redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -28,7 +28,7 @@ export async function createInvoice(formData: FormData) {
 
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date}
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
