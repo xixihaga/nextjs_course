02da1ffdac0c8b72d80287962d19a3de9a91c18d
xixihaga <czm9790_cn@hotmail.com>
2024-04-15 08:44:43 +0800

    Chapter 12 ++++

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,25 @@
 import Form from '@/app/ui/invoices/edit-form';
 import { invoices } from "@/app/lib/placeholder-data";
-import  Breadcrumbs  from '@/app/ui/invoices/breadcrumbs';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
-export default async function Page({params}: {params:{id:string}}) {
+export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers()
     ])
-    return(
-        <main> 
+
+    if (!invoice) {
+        notFound();
+    }
+
+    return (
+        <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    {label: 'Invoices', href: '/dashboard/invoices'},
+                    { label: 'Invoices', href: '/dashboard/invoices' },
                     {
                         label: "Edit Invoice",
                         href: `/dashboard/invoices/${id}/edit`,
