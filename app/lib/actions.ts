5accfbaf79f0ace7a823bef6806eafcfd8ad27df
xixihaga <czm9790_cn@hotmail.com>
2024-04-18 21:05:24 +0800

    Chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { formatDateToLocal } from './utils';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -85,12 +87,30 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoice WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices')
         return {message: 'Delete Invoice.'}
     } catch (error) {
         return {message: "Datebase Error: Failed to Delete Invoice."}
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -137,9 +137,9 @@ export default function Form({
       </div>
       <div aria-live="polite" aria-atomic="true">
         {
-          state.message ? (
+          state.message && (
             <p className="mt-2 text-sm text-red-500">{state.message}</p>
-          ) : null
+          )
         }
       </div>
       <div className="mt-6 flex justify-end gap-4">
